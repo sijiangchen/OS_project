2334c8dbe4e0407130d8d0cb94aab70eea1be083
sijiangchen <chens32@rpi.edu>
2020-04-17 07:46:59 -0400

    Update Output.cpp

--- Output.cpp
+++ Output.cpp
@@ -5,7 +5,13 @@
 #include "Output.h"
 
 Output::Output(string name) {
-    this->name=name;
+  this->name=name;
+      CPUBurstTime=0;
+      WaitTime=0;
+      TurnaroundTime=0;
+      contextSwitch=0;
+      preemption=0;
+
 }
 
 double Output::getCPUBurstTime() {
@@ -47,11 +53,22 @@ void Output::addContextSwitch() {
 void Output::addPreemption() {
     this->preemption+=1;
 }
-
+/*
 void Output::print() {
     cout<<"total cpu burst time: "<<this->CPUBurstTime<<endl;
     cout<<"total wait time: "<<this->WaitTime<<endl;
     cout<<"total Turnaround time: "<<this->TurnaroundTime<<endl;
     cout<<"total context switch: "<<this->contextSwitch<<endl;
     cout<<"total preemption: "<<this->preemption<<endl;
+}
+*/
+
+ostream& Output::print(ostream& out_str) {
+    out_str<<setiosflags(ios::fixed)<<setprecision(3);
+    out_str<<"-- average CPU burst time: "<<this->CPUBurstTime/this->contextSwitch<<" ms"<<endl;
+    out_str<<"-- average wait time: "<<this->WaitTime/this->contextSwitch<<" ms"<<endl;
+    out_str<<"-- average turnaround time: "<<this->TurnaroundTime/this->contextSwitch<<" ms"<<endl;
+    out_str<<"-- total number of context switches: "<<this->contextSwitch<<endl;
+    out_str<<"-- total number of preemptions: "<<this->preemption<<endl;
+    return out_str;
 }
